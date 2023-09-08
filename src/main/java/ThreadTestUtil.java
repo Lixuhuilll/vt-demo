2c233797c59e0ff53623577588581b5908b490a8
lixuhuilll <676824363@qq.com>
2023-09-08 23:21:34 +0800

    优化

--- src/main/java/ThreadTestUtil.java
+++ src/main/java/ThreadTestUtil.java
@@ -8,16 +8,29 @@ public class ThreadTestUtil {
     private static final Logger log = LoggerFactory.getLogger(ThreadTestUtil.class);
 
     public static void threadTest(ThreadFactory factory) {
-        long start = System.currentTimeMillis();
 
+        Thread[] threads = new Thread[10_0000];
+
+        long create = System.currentTimeMillis();
         for (int i = 0; i < 10_0000; i++) {
-            factory.newThread(() -> {
+            threads[i] = factory.newThread(() -> {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException ignored) {}
-            }).start();
+            });
         }
+        log.info("创建用时：{} ms", System.currentTimeMillis() - create);
 
-        log.info("用时：{} ms", System.currentTimeMillis() - start);
+        long start = System.currentTimeMillis();
+        for (var thread : threads) {
+            thread.start();
+        }
+        // 等待执行完成
+        try {
+            for (var thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException ignored) {}
+        log.info("运行用时：{} ms", System.currentTimeMillis() - start);
     }
 }
